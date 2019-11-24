57cbddb1bb1d17002ba5320c2f18de37724889d8
Jan Jenko <jan.jenko94@gmail.com>
2019-11-24 19:42:52 +0100

    Multiple LFSRs

--- reversibility.py
+++ reversibility.py
@@ -1,98 +1,58 @@
 import itertools
 
-def xor_combine(sez):
-    while len(sez) > 1:
-        a = sez.pop()
-        b = sez.pop()
-        
-        sez.append(a ^ b)
-    
-    return sez.pop()
-    
 
-#input is list of 0 and 1 where input[0] = x^max
-#exponents are indexes of which indexes we should use
-def lfsr(input, exponents):
-        
-        #values of the input that we will xor
-        values = [input[i] for i in exponents]
-        
-        #shift the input bits by 1 to the right and insert a combined xor at the beginning
-        return (xor_combine(values), input[0], input[1], input[2], input[3], input[4], input[5], input[6])
+def compute(polynomial, input_register):
+    output = len(input_register) * [None]
+    
+    output[1:] = input_register[:-1]
+
+    xor = 0
+    for i in range(len(polynomial)):
+        if polynomial[i] == 1:
+            xor ^= input_register[i]
+
+    output[0] = xor
+
+    return output
 
 
-#generates all possible polynomials or rather creates all the possible combinations of chosen exponents
-def all_polynomials():
-    
-    #iterate from 1 to 255
-    for i in range(1,256):
-    
-        #convert to binary and chop away the starting 0b notation
-        binary = [int(x) for x  in bin(i).split("0b")[1]]
-        
-        #bin converts number to binary without leading 0s
-        #we work with 8 bits so we want to add leading 0s to any number that doesn't have them
-        while len(binary) < 8:
-            binary.insert(0, 0)
-        
-        #now we have to extract on which index the 1s appear
-        indexes = [i for i in range(len(binary)) if binary[i] == 1]
-        
-        #we have the indexes of the exponents, yield them now
-        yield indexes
+registers = list(itertools.product([0, 1], repeat=8))
+polynomials = set(itertools.permutations([0, 0, 0, 0, 0, 1, 1, 1]))
 
-def check_cycles(inputs, outputs):
-    
-    #slovar = {input[i]: output[i] for i in range(len(inputs))}
-    slovar = dict()
-    for i in range(len(inputs)):
-        slovar[inputs[i]] = outputs[i]
-    starting_input = inputs[0]
-    current_input = slovar[starting_input]
-    cycle_len = 1
-    
-    while current_input != starting_input:
-        cycle_len += 1
-        current_input = slovar[current_input]
-    
-    return cycle_len
-    
-    
 
-if __name__ == "__main__":
-    x = [True, False]
-    all_inputs = list(itertools.product(x, repeat=8))
-    
-    reversible_polynomials = []
-    
-    #iterate over all the polynomials
-    for exponents in all_polynomials():
-        
-        #all_outputs = set()
-        all_outputs = []
-        
-        #iterate over all the inputs
-        for input in all_inputs:
-            
-            #calculate one lfsr shift for the given input and the given polynomial
-            output = lfsr(input, exponents)
-            
-            #add the output to the set
-            #all_outputs.add(output)
-            all_outputs.append(output)
-        
-        
-        #print(len(all_outputs), len(list(set(all_outputs))))
-        
-        #now check if the lfsr with a given polynomial is reversable => has the same number of unique outputs as inputs
-        if len(list(set(all_outputs))) == len(all_inputs):
-            cycle_length = check_cycles(all_inputs, all_outputs)
-            if cycle_length == 255:
-                reversible_polynomials.append(exponents)
-                print("The polynomial with the exponents:", exponents, "is reversible")
-                print("The size of the cycle is:", cycle_length)
-                print("-------------------------------------------------------------------------------")
-    
-    
-    print("*******************************************************************************")
-    print("Out of a total of 255 polynomials,", len(reversible_polynomials), "are reversible and have a cycle of length 255")
+k = 0
+for polynomial in polynomials:
+
+    # First check for reversibility
+    outputs = set()
+    for register in registers:
+        outputs.add(tuple(compute(polynomial, register)))
+
+    reversible = len(outputs) == len(registers)
+
+    if not reversible:
+        continue
+
+    print(polynomial)
+
+    # Check for cycle lengths
+    cycle_lengths = set()
+    for register in registers:
+
+        temp_register = list(register)
+
+        i = 0
+        while True:
+            i += 1
+            temp_register = compute(polynomial, temp_register)
+
+            if tuple(temp_register) == register:
+                cycle_lengths.add(i)
+                break
+
+    print(cycle_lengths)
+    if 255 in cycle_lengths:
+        k += 1
+    print('---------------------')
+
+print(k)
